786ef75c5e222d68cd37bd8194cbe95e1658b635
Robin Shi <bin.shi@dfactory.tech>
2022-05-09 17:17:55 +0800

    fix error issue 8

--- src/mapping.ts
+++ src/mapping.ts
@@ -52,12 +52,12 @@ export function handleNewAdmin(event: NewAdmin): void {
 }
 
 export function handleNewImplementation(event: NewImplementation): void {
-  let entity = Pool.load(event.transaction.to.toHex())
+  let entity = Pool.load(event.transaction.from.toHex())
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
   if (!entity) {
-    entity = new Pool(event.transaction.to.toString())
+    entity = new Pool(event.transaction.from.toHex())
   }
 
   // Entity fields can be set based on event parameters
