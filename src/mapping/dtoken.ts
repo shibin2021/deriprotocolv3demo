f32e9fb51865f55009f99aa4ac0856789d80603a
Robin Shi <bin.shi@dfactory.tech>
2022-05-25 22:28:16 +0800

    fix error issue 50

--- src/mapping/dtoken.ts
+++ src/mapping/dtoken.ts
@@ -1,20 +1,20 @@
 import {
   Transfer,
 } from "../../generated/DToken/DToken"
-import { DToken } from "../../generated/schema"
+import { DToken, Pool } from "../../generated/schema"
 import { POOL_ADDRESS } from "./helper"
 
 export function handleDTokenTransder(event: Transfer): void {
   const emitter = event.transaction.from
   const from = event.params.from.toHexString()
   const to = event.params.to
-  const id = event.params.tokenId
+  const id = event.params.tokenId.toString()
   let entity = DToken.load(id)
   let pool = Pool.load(POOL_ADDRESS)
   if(!entity) {
     entity = new DToken(id)
   }
-  entity.type = pool.PTokenAddress === emiter ? 'PToken' : 'LToken'
+  entity.type = pool.PTokenAddress === emitter ? 'PToken' : 'LToken'
   entity.account = to
   entity.timestamp = event.block.timestamp.toI32()
   entity.save()
