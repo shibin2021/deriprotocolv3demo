7e846b62c0341ecd941d0d07bcf9819a811a8ac4
Robin Shi <bin.shi@dfactory.tech>
2022-06-02 16:47:06 +0800

    fix 68

---
+++ src/mapping/symbol_manager.ts
@@ -0,0 +1,30 @@
+import { Trade } from "../../generated/SymbolManagerImplementation/SymbolManagerImplementationAbi"
+import { getOrInitPosition, getOrInitSymbolManager, getOrInitTradeHistory } from "../helpers/initializers"
+
+// pTokenId, symbolId, indexPrice,tradeVolume,tradeCost,tradeFee
+export function handlePoolTrade(event: Trade): void {
+  const pTokenId = event.params.pTokenId
+  const symbolId = event.params.symbolId
+  let tradeHistory = getOrInitTradeHistory(pTokenId, symbolId, event)
+  tradeHistory.symbolId = symbolId
+  tradeHistory.symbol = ""
+  tradeHistory.pTokenId = pTokenId
+  tradeHistory.indexPrice = event.params.indexPrice
+  tradeHistory.tradeVolume = event.params.tradeVolume
+  tradeHistory.tradeCost = event.params.tradeCost
+  tradeHistory.tradeFee = event.params.tradeFee
+  tradeHistory.timestamp = event.block.timestamp.toI32()
+  const symbolManager = getOrInitSymbolManager(event.address)
+  tradeHistory.pool = symbolManager.pool
+  tradeHistory.save()
+
+  let position = getOrInitPosition(pTokenId, symbolId, event)
+  position.symbolId = symbolId
+  position.symbol = ""
+  position.pTokenId = pTokenId
+  position.volume = position.volume.plus(event.params.tradeVolume)
+  position.timestamp = event.block.timestamp.toI32()
+  position.pool = symbolManager.pool
+  const account = event.transaction.from
+  position.save()
+}
